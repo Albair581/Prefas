f3369fbc34795017b292ad32607758cbc9f37f88
Albair581 <148535174+Albair581@users.noreply.github.com>
2024-07-29 07:26:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -404,11 +404,11 @@ if st.session_state.page == 2:
             abtype = st.selectbox("請假類別", 
                                 ["特休", "境外", "病假", "事假", "喪假", "婚假", "生育假"])
 
-        absdate = st.date_input("請假開始日期", value=datetime.now().replace(hour=0, minute=0, second=0, microsecond=0))
+        absdate = st.date_input("請假開始日期", value=datetime.now().date())
         abstime = st.time_input("請假開始小時 (24時制)")
         abstime = str(abstime)
 
-        abedate = st.date_input("請假結束日期", value=datetime.now().replace(hour=0, minute=0, second=0, microsecond=0))
+        abedate = st.date_input("請假結束日期", value=datetime.now().date())
         abetime = st.time_input("請假結束小時 (24時制)")
         abetime = str(abetime)
 
